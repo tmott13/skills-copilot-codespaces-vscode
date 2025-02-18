b946f6d9de99155dab326642ba3ec100e6221b57
Tanya Mott <mott.tanya13@gmail.com>
2025-02-18 00:03:46 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,8 @@
 // create web server 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
-
-// create a file to store comments
-var comments = [];
-var commentsFile = 'comments.json';
-
-// read comments from file
-fs.readFile(commentsFile, function(err, data) {
-  if (err) {
-    console.log('Error reading comments file: ' + err);
-  } else {
-    comments = JSON.parse(data);
-  }
-});
+// create a route
+// create a function that will handle the route
+// create a function that will return the comments
+// create a function that will add a comment
+// create a function that will delete a comment
+// create a function that will update a comment
+// create a function that will return a comment
